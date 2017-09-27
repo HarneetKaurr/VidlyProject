98a47c85d704eb43b9f6d2ce1c2e012625edccd8
Harneet <harneetkaur0519@gmail.com>
2017-09-27 06:39:44 -0400

    Section3-Execise3-

--- VidlyMVC/Controllers/MoviesController.cs
+++ VidlyMVC/Controllers/MoviesController.cs
@@ -3,47 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using System.Web.UI.WebControls;
+using System.Data.Entity;
 using VidlyMVC.Models;
-using VidlyMVC.ViewModels;
-
 namespace VidlyMVC.Controllers
 {
     public class MoviesController : Controller
     {
         // GET: Customer
+        private ApplicationDbContext _context;
+
+        public MoviesController()
+        {
+            _context = new ApplicationDbContext();
+        }
         public ViewResult Index()
         {
-            var movies = GetMovies();
-
+            var movies = _context.Movies.Include(c => c.Genre).ToList();
             return View(movies);
         }
 
-        private IEnumerable<Movie> GetMovies()
+        public ActionResult Details(int id)
         {
-            return new List<Movie>
-            {
-                new Movie {Id = 1, Name = "Ring"},
-                new Movie {Id = 2, Name = "Hell"}
-            };
+            var movies = _context.Movies.Include(c => c.Genre).SingleOrDefault(c => c.Id == id);
+            return View(movies);
         }
 
-        public ActionResult Random()
-        {
-            var movie = new Movie() { Name = "Shrek!" };
-            var customers = new List<Customer>
-            {
-                new Customer { Name = "Customer 1" },
-                new Customer { Name = "Customer 2" }
-            };
-
-            var viewModel = new RandomMovieViewModel
-            {
-                Movie = movie,
-                Customers = customers
-            };
-
-            return View(viewModel);
-        }
     }
 }
