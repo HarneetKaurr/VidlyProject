4104818ff907194aafc56c67af5ab8d64044e6e5
Harneet <harneetkaur0519@gmail.com>
2017-09-27 03:57:23 -0400

    Section2-Exercise

--- VidlyMVC/Controllers/MoviesController.cs
+++ VidlyMVC/Controllers/MoviesController.cs
@@ -11,14 +11,30 @@ namespace VidlyMVC.Controllers
 {
     public class MoviesController : Controller
     {
-        // GET: Movies
+        // GET: Customer
+        public ViewResult Index()
+        {
+            var movies = GetMovies();
+
+            return View(movies);
+        }
+
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie {Id = 1, Name = "Ring"},
+                new Movie {Id = 2, Name = "Hell"}
+            };
+        }
+
         public ActionResult Random()
         {
-            var movie = new Movie() {Name="Shrek"};
+            var movie = new Movie() { Name = "Shrek!" };
             var customers = new List<Customer>
             {
-                new Customer {Name = "John"},
-                new Customer {Name = "Daves"}
+                new Customer { Name = "Customer 1" },
+                new Customer { Name = "Customer 2" }
             };
 
             var viewModel = new RandomMovieViewModel
@@ -26,28 +42,8 @@ namespace VidlyMVC.Controllers
                 Movie = movie,
                 Customers = customers
             };
+
             return View(viewModel);
-        }
-
-        public ActionResult Edit(int id)
-        {
-            return Content("id=" + id);
-        }
-
-        public ActionResult Index(int? pageIndex, string sortBy)
-        {
-            if (!pageIndex.HasValue)
-                pageIndex = 1;
-            if (string.IsNullOrEmpty(sortBy))
-                sortBy = "Name";
-
-            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
-
-        }
-        [Route("movies/released/{year}/{month}")]
-        public ActionResult ByReleaseDate(int year,int month)
-        {
-            return Content(year+"/"+month);
         }
     }
 }
