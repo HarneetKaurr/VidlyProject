2e3a30fc040ecdfed426cef3591da2a38683123f
Harneet <harneetkaur0519@gmail.com>
2017-10-05 02:10:48 -0400

    Web Api

---
+++ VidlyMVC/Dtos/CustomerDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using VidlyMVC.Models;
+
+namespace VidlyMVC.Dtos
+{
+    public class CustomerDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public bool IsSubscribedToNewLetter { get; set; }
+
+        public byte MembershipTypeId { get; set; }
+        public DateTime? BirthDate { get; set; }
+    }
+}
--- VidlyMVC/Global.asax.cs
+++ VidlyMVC/Global.asax.cs
@@ -2,9 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using AutoMapper;
+using VidlyMVC.App_Start;
 
 namespace VidlyMVC
 {
@@ -12,6 +15,8 @@ namespace VidlyMVC
     {
         protected void Application_Start()
         {
+            Mapper.Initialize(c => c.AddProfile<MappingProfile>());
+            GlobalConfiguration.Configure(WebApiConfig.Register);
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
